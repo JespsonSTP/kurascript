fd2e500943b8e108f6f5b4ab8b5051b6cef4838c
Jespson <jespstpierre@yahoo.com>
2022-08-16 01:57:04 -0400

    added ranges to the grades

--- kuracsv.py
+++ kuracsv.py
@@ -15,21 +15,84 @@ diagnostic_num=getpass.getpass(prompt="Enter diagnostic num: ")
 smtp_obj.login(email, password)
 
 #admin email for cc 
+"""
 admin1 = getpass.getpass(prompt="Enter email: ") 
 admin2 = getpass.getpass(prompt="Enter email: ") 
 admin3 = getpass.getpass(prompt="Enter email: ") 
 admin4 = getpass.getpass(prompt="Enter email: ") 
 
+def passing_send_email(email, stu_email, diagnostic_num, stu_name, stu_grade, stu_out_of_five, admin1, admin2, admin3, admin4):
+
+"""
+
 #function to send email
-def send_email(email, stu_email, diagnostic_num, stu_name, stu_grade, admin1, admin2, admin3, admin4):
+def passing_send_email(email, stu_email, diagnostic_num, stu_name, stu_grade, stu_out_of_five):
     #This function contains all the information needed to send an email using smtp
     msg = EmailMessage()
-    msg.set_content(f"Hi {stu_name}, Congratulations for completing diagnotic {diagnostic_num}. Here is your score {stu_grade}")
+    msg.set_content(f"""
+Hello {stu_name}, 
+
+Below you will find your total score as well as the score for each question for diagnostic {diagnostic_num}.
+
+Congratulations on earning a score of {stu_grade}, which meets the passing benchmark of 85%. 
+
+We encourage you to continue to study, practice and, as always, seek out opportunities to help your classmates who may have questions about these and other topics. Remember, the more you teach, the more you learn and improve your understanding. 
+
+Total Score: {stu_grade}
+{stu_out_of_five}""")
 
     msg['Subject'] = f"score for diagnotic {diagnostic_num}"
     msg['From'] = email
     msg['To'] = stu_email
-    msg['cc'] = [admin1, admin2, admin3, admin4]
+    #msg['cc'] = [admin1, admin2, admin3, admin4]
+    smtp_obj.send_message(msg)
+
+def failling_send_email(email, stu_email, diagnostic_num, stu_name, stu_grade, stu_out_of_five):
+    #This function contains all the information needed to send an email using smtp
+    msg = EmailMessage()
+    msg.set_content(f"""
+Hello {stu_name}, 
+
+Below you will find your total score as well as the score for each question for diagnostic {diagnostic_num}.
+     
+You earned a score of {stu_grade}, which does not meet the passing benchmark of 85%. 
+
+Do not be discouraged as you are certainly capable of learning this material but, as you can see, you will really need to dedicate more time to learning and practicing. We would highly recommend that you seek to retake this assignment by first reviewing the class notes, videos, online resources, and setting up/joining study groups with your classmates. If, after doing all of this, the answers are still not clear, then you should send out a request for an office hour with an instructor or learning coach. 
+If after your first retake you do not pass again, we will want to set up a dedicated review session with you to understand where you may be encountering challenges so that we can help address them. 
+    
+Do not get discouraged! It’s time to rise to meet the challenge!
+
+Total Score: {stu_grade}
+{stu_out_of_five}""")
+
+    msg['Subject'] = f"score for diagnotic {diagnostic_num}"
+    msg['From'] = email
+    msg['To'] = stu_email
+    #msg['cc'] = [admin1, admin2, admin3, admin4]
+    smtp_obj.send_message(msg)
+
+
+def between_50_and_84_send_email(email, stu_email, diagnostic_num, stu_name, stu_grade, stu_out_of_five):
+    #This function contains all the information needed to send an email using smtp
+    msg = EmailMessage()
+    msg.set_content(f"""
+Hello {stu_name}, 
+
+Below you will find your total score as well as the score for each question for diagnostic {diagnostic_num}.
+     
+You earned a score of {stu_grade}, which does not meet the passing benchmark of 85%. 
+
+Do not be discouraged as you are certainly within range of passing. We would highly recommend that you seek to retake this assignment by first reviewing the class notes, videos, online resources, and setting up/joining study groups with your classmates. If, after doing all of this, the answers are still not clear, then you should send out your question(s) to an instructor or learning coach and they can decide whether it warrants an office hour. 
+
+Don’t give up, you're almost there!
+    
+Total Score: {stu_grade}
+{stu_out_of_five}""")
+
+    msg['Subject'] = f"score for diagnotic {diagnostic_num}"
+    msg['From'] = email
+    msg['To'] = stu_email
+    #msg['cc'] = [admin1, admin2, admin3, admin4]
     smtp_obj.send_message(msg)
 
 csvfile = open('testing1.csv', 'r')
@@ -40,8 +103,15 @@ for row in csv.DictReader(csvfile):
     #print(row)
     # Print the dictionary keys
     #print(row.keys())
-    print(row['first name '], row['grade '], row['email'])
-    send_email(email, row['email'], diagnostic_num, row['first name '], row['grade '], admin1, admin2, admin3, admin4)
+    if int(row['perc_grade']) < 50:
+        failling_send_email(email, row['email'], diagnostic_num, row['first name'], row['grade'], row['Score out of 5'])
+    elif int(row['perc_grade']) > 50 and int(row['perc_grade']) < 84:
+        between_50_and_84_send_email(email, row['email'], diagnostic_num, row['first name'], row['grade'], row['Score out of 5'])
+    else:
+        passing_send_email(email, row['email'], diagnostic_num, row['first name'], row['grade'], row['Score out of 5'])
+    
+    
 
 
 smtp_obj.quit()
+
